ac71abbc595063fac621fb49beee170cb59a8b9d
Ayman <aymanm95@gmail.com>
2017-07-08 20:05:11 +0100

    Synchronisation methods added

--- OpenMP/pi-sync.c
+++ OpenMP/pi-sync.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
 #include <omp.h>
 
-static long num_steps = 1000000000;
+static long num_steps = 100000000;
 double step;
 
 #define NUM_THREAD 4
-#define PARALLEL 0
-#define PAD 8 //Elimate false sharing (Assuming 64 byte cacheline size)
+#define PARALLEL 1
 
 int main() {
+
+	/* Same code but with synchronisation methods to eliminate ugly false sharing guards */
+
+	// Mutex - critical
 
 	step = 1.0/(double) num_steps;
 	int i;
 	double pi=0.0;
 
+	
 	#ifdef PARALLEL
 
 	int act_threads;
-	double sum[NUM_THREAD][PAD];	
-	omp_set_num_threads(NUM_THREAD);	
+	
+	omp_set_num_threads(NUM_THREAD);
+	
 	#pragma omp parallel 
 	{
 		int i;
+		double sum;
 		int id = omp_get_thread_num();
 		int req_threads = omp_get_num_threads();
 		double x;
 		if (id == 0) {		
 			act_threads = req_threads;
 		}
-		for (i=id, sum[id][0]=0.0; i<num_steps; i+=req_threads) {
+		for (i=id,sum=0.0; i<num_steps; i+=req_threads) {
 			x = (i+0.5) * step;
-			sum[id][0] += 4.0/(1.0 + x*x);
+			sum += 4.0/(1.0 + x*x);
 		}
-	}
-	for (int i=0; i<act_threads; i++) {
-		pi += step * sum[i][0];
+		#pragma omp critical 
+		{
+			pi += step * sum;
+		}
 	}
 
 	// Serial code
@@ -43,9 +50,10 @@ int main() {
 		for (i=0; i<num_steps; i++) {
 			x = (i+0.5) * step;
 			sum += 4.0/(1.0 + x*x);		
-		}
+		}	
 		pi = step * sum;
 	#endif	
+	
 	printf("pi = %f\n",pi);
 	return 0;
 }
